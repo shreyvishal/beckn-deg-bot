b0bb71aee7ad758dbcb334132815d4af14b10107
shreyvishal <shreya.vishal@eminds.ai>
2025-06-26 14:46:35 +0530

    Added: Initial Changes for branch

--- src/controllers/ai_controllers.py
+++ src/controllers/ai_controllers.py
@@ -1,5 +1,8 @@
+import json
+import logging
+from typing import Any, cast
 from unicodedata import category
-from langchain_core.runnables import RunnableBranch, RunnableLambda
+from langchain_core.runnables import RunnableBranch, RunnableLambda, RunnableMap
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -8,41 +11,63 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 from service.ai.chat_history import with_session_memory
-from service.ai.prompts_and_model import categorier_model, general_chat_model, general_prompt_template, intent_categorier_prompt_template
+from service.ai.prompts_and_model import categorier_model, domain_categoriser_prompt_template, general_chat_model, general_prompt_template, intent_categorier_prompt_template
 
 load_dotenv()
 
 
 def log_beckn(x):
-    print("Input as X in BECKN_TRANSACTION", x)
+    logging.info("Input as X in BECKN_TRANSACTION", x)
     return x
+
+
+
+
+
+
+def domain_categoriser_chain(data):
+    return( domain_categoriser_prompt_template 
+        | categorier_model 
+        | StrOutputParser() 
+        | RunnableLambda( lambda x: 
+           {"domain":x,"input":data["input"],"category":data["category"], "chat_history":data["chat_history"]}
+            ))
+
+
+
 
 branches = RunnableBranch(
     (
         lambda x: isinstance(x, dict) and "BECKN_TRANSACTION" in x["category"],
-        RunnableLambda(lambda x: isinstance(x, dict) and{**x, "output":x["input"]})
-    ),general_prompt_template | general_chat_model | StrOutputParser()
+        RunnableLambda(lambda x: domain_categoriser_chain(x))
+        | RunnableBranch(
+            (
+                lambda x: "deg:retail" in x["domain"] , # type: ignore
+                RunnableLambda(lambda x: print("In deg:retail-----> ",x) or x)
+                | general_prompt_template | general_chat_model | StrOutputParser()
+            ),
+            (
+                lambda x: "deg:schemes" in x["domain"] , # type: ignore
+                RunnableLambda(lambda x: print("In deg:schemes-----> ",x) or x)
+                | general_prompt_template | general_chat_model | StrOutputParser()
+            ),    
+            RunnableLambda(lambda x: {"output":x})
+            
+        )
+        
+    ),
+    RunnableLambda(lambda x: print("In General-----> ",x) or x)
+    | general_prompt_template | general_chat_model | StrOutputParser()
     
 )
 
 
 
 
-
-
-def ai_health_check_controller():
-    """Health check endpoint for AI service"""
-    return {
-        "status":"healthy",
-        "message":"AI service is running"
-    }
-
 def ai_chat_controller(message: str, session_id: str = "default-session"):
     """Chat endpoint for AI service"""
-    
-    intent_categorier_chain = intent_categorier_prompt_template | categorier_model | StrOutputParser() | RunnableLambda(lambda x: {"category":x, "input":message})
-    
-    intent = intent_categorier_chain.invoke({"input":message})
+    intent_categoriser_chain = intent_categorier_prompt_template | categorier_model | StrOutputParser() | RunnableLambda(lambda x: { "category":x, "input":message})
+    intent = intent_categoriser_chain.invoke({"input":message})
     
     print("intent",intent)
     
@@ -52,8 +77,19 @@ def ai_chat_controller(message: str, session_id: str = "default-session"):
     
     main_chain_with_memory = with_session_memory(main_chain)
     data = main_chain_with_memory.invoke({"input": intent["input"], "category":intent["category"]}, config={"configurable":{"session_id":session_id}})
-    print("data",data)
+   
+   
+    print("\n\ndata",data)
     return {
         "status":"success",
         "message":data
+    }
+    
+    
+    
+def ai_health_check_controller():
+    """Health check endpoint for AI service"""
+    return {
+        "status":"healthy",
+        "message":"AI service is running"
     }
--- src/service/ai/prompts_and_model.py
+++ src/service/ai/prompts_and_model.py
@@ -3,17 +3,24 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
 
+global_system_instruction = """You are Luma an AI Agent that is capable to perform:
+        - Beckn Open Network transactions for energy domains and their subdomains are
+            - Retail: Buy retail energy appliance eg. Battery, Solar Panel, etc.
+            - Schemes: Subscribe to energy schemes eg. DFP (Demand Flexibility Program) and get discounts on energy consumption
+        - Answer general queries on any topic
+        - If the user greets you then, greet the user, by introducing yourself introduce the user to your capabilities 
+        """
+
 general_chat_model = ChatOpenAI(model="gpt-4o", temperature=0.6)
 
 categorier_model = ChatOpenAI(model="gpt-4o", temperature=0)
 
 
+
+
 intent_categorier_prompt_template = ChatPromptTemplate.from_messages([
-    ("system", """You are a categorier agent that is capable to categorize the user's message into a category.
-        - The user's message: {input}
-        - The category:
+    ("system", """You are Luma an AI Agent that is capable to perform Beckn Open Network transactions and answer general queries. categorier agent that is capable to categorize the user's message into a category.
         """),
-    # ("placeholder", "{chat_history}"),
     ("user", """
      User will send you a query and you will categorize the query into a category on below criteria:
      - If the query is about energy then it is a energy query
@@ -29,6 +36,31 @@ intent_categorier_prompt_template = ChatPromptTemplate.from_messages([
      """),
 ])
 
+domain_categoriser_prompt_template = ChatPromptTemplate.from_messages([
+    ("system", global_system_instruction),
+    ("placeholder", "{chat_history}"),
+    ("user", """
+     User will provide a message to you, and you will also be provided with the chat history with the user. Analyse the chat history and the user's message and categorize the user's message into a category on below criteria:    
+     - If the user is asking to search for a product then return "deg:retail"
+     - If the user is asking to search for a scheme (eg: demand flexibility program(DFP), demand side management program, discount on energy consumption, etc) then return "deg:schemes"
+     
+     - If the user is asking to select or pick or choose a product from the list that you can retrieve from the chat history then return "deg:retail"
+     - If the user is asking to select or pick or choose a scheme or program from the list that you can retrieve from the chat history then return "deg:schemes"
+     
+     
+     - If the user is asking to confirm an order or transaction for a product from the list that you can retrieve from the chat history then return then return "deg:retail"
+     - If the user is asking to confirm an order or transaction for a scheme or program or subscribe to a program from the list that you can retrieve from the chat history then return then return "deg:schemes"
+     
+     
+     **NOTE:**
+     - Only return the domain name, do not return any other text or explanation
+     
+     User's message: {input}
+     AI Response:
+     """),
+])
+
+
 
 
 
@@ -36,13 +68,7 @@ intent_categorier_prompt_template = ChatPromptTemplate.from_messages([
 
 
 general_prompt_template = ChatPromptTemplate.from_messages([
-    ("system", """You are Luma an AI Agent that is capable to perform:
-        - Beckn Open Network transactions for energy domains and their subdomains are
-            - Retail: Buy retail energy appliance eg. Battery, Solar Panel, etc.
-            - Schemes: Subscribe to energy schemes eg. DFP (Demand Flexibility Program) and get discounts on energy consumption
-        - Answer general queries on any topic
-        - If the user greets you then, greet the user, by introducing yourself introduce the user to your capabilities 
-        """),
+    ("system", global_system_instruction),
     ("placeholder", "{chat_history}"),
     ("user", """
      User will provide a message to you, and you will respond to the user based on your capabilities and the user's message
